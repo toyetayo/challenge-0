05d13c362bd1b7622dbfdcf4691ae2dc2b50f7c3
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-05-09 13:29:44 -0500

    Request input from user

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # **Challenge0** 
+# *After tax Calaculations*
 This code calculates the aftertax value of dollar amount inputted by cursor.
 
-
+# Displays a comment based on the amount of grand_total
 A comment is added to the grand_total output based on this "If the grand total is equal to or less than $5.00 then message should be: "Pocket Change"
 If the grand total is greater than $5.00 but less than $20 the message should be: "Wallet Time"
 If the grand total is equal to or greater than $20 the message should be: "Charge It!".
 
-
+# Request input from User for sub_total
+This takes sub-total input from the user, then calculates the grand total after tax based on the user input after which it outputs a message based on the grand_total amount. 
 
--- challenge0.rb
+++ challenge0.rb
@@ -19,10 +19,36 @@ puts "PST: $#{'%.2f' % pst_amount} - #{(PST * 100).to_i}%"
 puts "GST: $#{'%.2f' % gst_amount} - #{(GST * 100).to_i}%"
 puts "Grand Total: $#{'%.2f' % grand_total}"
 
-#2. beloe prints a short message after the grand_total
+#2. below prints a short message after the grand_total
 if grand_total <= 5.00
   puts "Pocket Change"
 elsif grand_total < 20.00
+  puts "Wallet Time"
+else
+  puts "Charge It!"
+end
+
+#3. Do some research online to figure out how to read keyboard input from Ruby and how to convert strings into floats.
+  
+# Input
+print "Enter the subtotal: "
+user_sub_total = gets.chomp.to_f
+ 
+# Calculations
+gst_amount = user_sub_total * GST
+pst_amount = user_sub_total * PST
+grand_total = user_sub_total + gst_amount + pst_amount
+ 
+# Output
+puts "Subtotal: $#{'%.2f' % user_sub_total}"
+puts "PST: $#{'%.2f' % pst_amount} - #{(PST * 100).to_i}%"
+puts "GST: $#{'%.2f' % gst_amount} - #{(GST * 100).to_i}%"
+puts "Grand Total: $#{'%.2f' % grand_total}"
+ 
+# Message
+if grand_total <= 5.00
+  puts "Pocket Change"
+elsif grand_total < 20
   puts "Wallet Time"
 else
   puts "Charge It!"
