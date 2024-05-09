0fc70dfff83f03b0cdaf4d3c16e36da291b4825b
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-05-08 21:49:20 -0500

    Average tax calculations

---
+++ challenge0.rb
@@ -0,0 +1,16 @@
+# Name      : Timothy Oyetayo
+# Course    : WEBD 3011 Agile Full stack Web Development
+# Ultimate Challenges  : Challenge 0
+
+sub_total = 5.00
+PST = 0.07
+GST = 0.05
+
+pst_amount = sub_total * PST
+gst_amount = sub_total * GST
+grand_total = pst_amount + gst_amount + sub_total
+
+puts "Subtotal:$#{'%.2f'% sub_total}"
+puts "PST: $#{'%.2f' % pst_amount} - #{(PST * 100).to_i}%"
+puts "GST: $#{'%.2f' % gst_amount} - #{(GST * 100).to_i}%"
+puts "Grand Total: $#{'%.2f' % grand_total}"
