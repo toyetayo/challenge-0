21879cfdba118be0cd2161b3a781e95c8ee7f960
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-05-14 15:08:15 -0500

    updated challenge0.rb

--- challenge0.rb
+++ challenge0.rb
@@ -4,7 +4,9 @@
 
 #1
 # declares a variablesub_total and contants PST and GST
-sub_total = 5.00
+print "Enter the subtotal: "
+sub_total = gets.chomp.to_f
+
 PST = 0.07
 GST = 0.05
 
@@ -14,6 +16,7 @@ gst_amount = sub_total * GST
 grand_total = pst_amount + gst_amount + sub_total
 
 #outputs the respective values of subtotal, PST< GST and grand_total
+#
 puts "Subtotal:$#{'%.2f'% sub_total}"
 puts "PST: $#{'%.2f' % pst_amount} - #{(PST * 100).to_i}%"
 puts "GST: $#{'%.2f' % gst_amount} - #{(GST * 100).to_i}%"
@@ -28,29 +31,5 @@ else
   puts "Charge It!"
 end
 
-#3. Do some research online to figure out how to read keyboard input from Ruby and how to convert strings into floats.
-  
-# Input
-print "Enter the subtotal: "
-user_sub_total = gets.chomp.to_f
- 
-# Calculations
-gst_amount = user_sub_total * GST
-pst_amount = user_sub_total * PST
-grand_total = user_sub_total + gst_amount + pst_amount
- 
-# Output
-puts "Subtotal: $#{'%.2f' % user_sub_total}"
-puts "PST: $#{'%.2f' % pst_amount} - #{(PST * 100).to_i}%"
-puts "GST: $#{'%.2f' % gst_amount} - #{(GST * 100).to_i}%"
-puts "Grand Total: $#{'%.2f' % grand_total}"
- 
-# Message
-if grand_total <= 5.00
-  puts "Pocket Change"
-elsif grand_total < 20
-  puts "Wallet Time"
-else
-  puts "Charge It!"
-end
+
 
